32eb0195a2f2b53e666b9d4bc85581f7988a40ba
kirin <kirin.chen1001@gmail.com>
2023-06-22 16:16:01 +0800

    Upgrade new version

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const webpack = require("webpack");
-const path = require("path");
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
-});
+module.exports = nextConfig
